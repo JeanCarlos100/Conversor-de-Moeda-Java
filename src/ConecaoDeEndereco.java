774aa6f22f0416d583c188e4a86e70bdafad08dc
Jean Carlos <santosjeancarlos70@gmail.com>
2024-06-12 19:44:33 -0300

    Atualicacao

---
+++ src/ConecaoDeEndereco.java
@@ -0,0 +1,4 @@
+public class ConecaoDeEndereco {
+
+    public convestoDeMoeda
+}
--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,28 @@
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Principal {
-    public static void main(String[] args) {
-        System.out.println("Teste");
+    public static void main(String[] args) throws IOException {
+        String endereco = "https://v6.exchangerate-api.com/v6/39cd47326a15613f1fed2705/mais recente/USD";
+
+        URL url = new URL(endereco);
+        HttpURLConnection request = (HttpURLConnection) url.openConnection();
+        request.connect();
+
+        JsonParser jp = new JsonParser();
+        JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+        JsonObject jsonobj = root.getAsJsonObject();
+
+        String req_result = jsonobj.get("result").getAsString();
+
+        System.out.println(req_result);
     }
 }
